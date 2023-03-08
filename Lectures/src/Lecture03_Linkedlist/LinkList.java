6e1f2d0c60e6ed4eb501a8908ebf07adfe9478f6
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-03-08 22:05:49 +0530

    Linked list done

--- Lectures/src/Lecture03_Linkedlist/LinkList.java
+++ Lectures/src/Lecture03_Linkedlist/LinkList.java
@@ -18,5 +18,42 @@ public class LinkList {
 			 current = current.next; 
 		 }
 	 }
+	 public boolean find(int key){
+		 Link current = first;
+		 
+		 while(!(current == null)) {
+			 if(current.iData == key) 
+				 return true;
+			 else 
+				 current = current.next;
+			 
+		 }
+		 
+		 return false;
+	 }
+	 
+	 public void insertFirst(int id) {
+		 Link newLink = new Link(id);
+		 newLink.next = first;
+		 first = newLink;
+	 }
+	 
+	 public Link deleteFirst() {
+		 Link temp = first;
+		 first = first.next;
+		 return temp;
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
 	
 }
